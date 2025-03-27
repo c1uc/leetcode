beb4b1245381ff6a46d60180560a83b97acb2615
Lin ,Rui-Teng <90602063+c1uc@users.noreply.github.com>
2025-03-27 12:53:25 +0800

    xx/xx/xx [10] Regular Expression Matching

---
+++ daily/10.regular-expression-matching.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=10 lang=cpp
+ *
+ * [10] Regular Expression Matching
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size(), m = p.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+        for(int j = 1; j <= m; j++) {
+            if(p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
+                } else if(p[j - 1] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] && s[i - 1] == p[j - 1];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
+// @lc code=end
+
