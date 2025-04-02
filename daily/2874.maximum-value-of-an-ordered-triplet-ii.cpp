cb7ca41b9f6132c81c5854684dd890f97f427170
c1uc <duck.cs13@nycu.edu.tw>
2025-04-02 10:24:07 +0800

    xx/xx/xx [2874] Maximum Value of an Ordered Triplet II

---
+++ daily/2874.maximum-value-of-an-ordered-triplet-ii.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=2874 lang=cpp
+ *
+ * [2874] Maximum Value of an Ordered Triplet II
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+        int high = nums[0];
+        long long ans = 0, diff = nums[0] - nums[1];
+        for (int i = 1; i < n - 1; ++i) {
+            if(nums[i - 1] > high) {
+                high = nums[i - 1];
+            }
+            if(high - nums[i] > diff) {
+                diff = high - nums[i];
+            }
+            ans = max(ans, diff * nums[i + 1]);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
