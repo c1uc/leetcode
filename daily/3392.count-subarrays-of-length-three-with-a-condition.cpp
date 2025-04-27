877ce957d375fdfd832a19f9998e65c61a58abf1
c1uc <duck.cs13@nycu.edu.tw>
2025-04-27 23:49:11 +0800

    2025/4/27	3392. Count Subarrays of Length Three With a Condition

---
+++ daily/3392.count-subarrays-of-length-three-with-a-condition.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        
+        for(int i = 0;i < n - 2;i++){
+            if((nums[i] + nums[i + 2]) * 2 == nums[i + 1]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
