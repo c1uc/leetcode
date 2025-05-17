357b29f663f77091bd375defd7633451e2d00429
c1uc <duck.cs13@nycu.edu.tw>
2025-05-17 14:39:11 +0800

    2025/5/17	75. Sort Colors

---
+++ daily/75.sort-colors.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0, j = 0, k = n - 1;
+        while (j <= k) {
+            if (nums[j] == 0) {
+                swap(nums[i++], nums[j++]);
+            } else if (nums[j] == 2) {
+                swap(nums[j], nums[k--]);
+            } else {
+                j++;
+            }
+        }
+        return;
+    }
+};
