aff1ba4260fe7eba9c7564537bb74e437211bf59
c1uc <90602063+c1uc@users.noreply.github.com>
2025-08-01 16:36:48 +0800

    2025/8/1	118. Pascal's Triangle

---
+++ daily/118.pascals-triangle.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+class Solution {
+    public:
+        vector<vector<int>> generate(int numRows) {
+            vector<vector<int>> ans(numRows);
+            for(int i = 0; i < numRows; i++) {
+                ans[i].resize(i + 1);
+                ans[i][0] = ans[i][i] = 1;
+                for(int j = 1; j < i; j++) {
+                    ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+                }
+            }
+            return ans;
+        }
+    };
