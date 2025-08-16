605e9b8ee4c81c55fc69b0c88a5bbf64fbff938c
c1uc <90602063+c1uc@users.noreply.github.com>
2025-08-16 22:59:53 +0800

    2025/8/16	1323. Maximum 69 Number

---
+++ daily/1323.maximum-69-number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+class Solution {
+    public:
+        int maximum69Number (int num) {
+            string s = to_string(num);
+            for (int i = 0; i < s.size(); i++) {
+                if (s[i] == '6') {
+                    s[i] = '9';
+                    break;
+                }
+            }
+            return stoi(s);
+        }
+    };
