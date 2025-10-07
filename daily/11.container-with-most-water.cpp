43c64e6d725935a70a2b08203e91b92f4331ae6a
c1uc <90602063+c1uc@users.noreply.github.com>
2025-10-07 09:27:32 +0800

    2025/10/4	11. Container With Most Water

---
+++ daily/11.container-with-most-water.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        int maxArea(vector<int>& height) {
+            int n = height.size();
+            int ans = 0, l = 0, r = n - 1;
+            while(l < r) {
+                ans = max(ans, min(height[l], height[r]) * (r - l));
+                if(height[l] > height[r]) r--;
+                else l++;
+            }
+            return ans;
+        }
+    };
