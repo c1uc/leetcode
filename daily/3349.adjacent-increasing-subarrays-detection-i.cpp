cda4698e9e5487e1203401b5c099c55bc0710189
c1uc <90602063+c1uc@users.noreply.github.com>
2025-10-14 09:17:03 +0800

    2025/10/14	3349. Adjacent Increasing Subarrays Detection I

---
+++ daily/3349.adjacent-increasing-subarrays-detection-i.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <ranges>
+using namespace std;
+
+
+class Solution {
+    public:
+        bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+            if(k == 1) return nums.size() > 1;
+            int len = 0, prev_len = 1;
+            for(auto x: nums | views::pairwise_transform([](auto a, auto b) {
+                return b > a;
+            })) {
+                if(x) {
+                    len++;
+                    if(len >= k && prev_len >= k || len >= (k << 1)) {
+                        return true;
+                    }
+                } else {
+                    prev_len = len;
+                    len = 1;
+                }
+            }
+            return false;
+        }
+    };
