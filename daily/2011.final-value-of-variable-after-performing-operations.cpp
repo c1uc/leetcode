599032ddcd48771410a323ec3be06b24b2e29800
c1uc <90602063+c1uc@users.noreply.github.com>
2025-10-20 15:20:31 +0800

    2025/10/20	2011. Final Value of Variable After Performing Operations

---
+++ daily/2011.final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    public:
+        int finalValueAfterOperations(vector<string>& operations) {
+            int ans = 0;
+            for(auto s: operations) {
+                if(s[0] == '+' || s[2] == '+') ans++;
+                else ans--;
+            }
+            return ans;
+        }
+    };
