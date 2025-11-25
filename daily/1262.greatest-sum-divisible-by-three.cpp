b48a8dd3baf14e6c2a26c17b477c7793d6d86853
c1uc <90602063+c1uc@users.noreply.github.com>
2025-11-25 12:56:26 +0800

    2025/11/23	1262. Greatest Sum Divisible by Three

---
+++ daily/1262.greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    public:
+        int maxSumDivThree(vector<int>& nums) {
+            vector<int> f = {0, INT_MIN, INT_MIN};
+            for(auto x: nums) {
+                vector<int> g = f;
+                for(int i = 0;i < 3;i++) {
+                    g[(i + x % 3) % 3] = max(g[(i + x % 3) % 3], f[i] + x);
+                }
+                f = g;
+            }
+            return f[0];
+        }
+    };
