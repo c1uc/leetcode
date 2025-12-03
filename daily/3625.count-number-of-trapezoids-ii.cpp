995076dc707580cedcdf1ad11e1a969e5cd75ccb
c1uc <90602063+c1uc@users.noreply.github.com>
2025-12-04 07:54:36 +0800

    2025/12/3	3625. Count Number of Trapezoids II

---
+++ daily/3625.count-number-of-trapezoids-ii.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    public:
+        int countTrapezoids(vector<vector<int>>& points) {
+            unordered_map<double, map<int, int>> slope_intercept_count;
+            unordered_map<int, map<int, int>> mid_slope_count;
+            for(int i = 0; i < points.size(); i++) {
+                int x1 = points[i][0], y1 = points[i][1];
+                for(int j = i + 1; j < points.size(); j++) {
+                    int x2 = points[j][0], y2 = points[j][1];
+                    int dx = x2 - x1, dy = y2 - y1;
+                    double slope = dx == 0 ? DBL_MAX : (double)dy / dx;
+                    double intercept = dx == 0 ? x1 : y1 - slope * x1;
+                    slope_intercept_count[slope][intercept]++;
+                    int mid = (x1 + x2 + 2000) << 12 | (y1 + y2 + 2000);
+                    mid_slope_count[mid][slope]++;
+                }
+            }
+            int ans = 0;
+            for(auto &[_, cnt] : slope_intercept_count) {
+                int s = 0;
+                for(auto &[_, c] : cnt) {
+                    ans += s * c;
+                    s += c;
+                }
+            }
+            for(auto &[_, cnt] : mid_slope_count) {
+                int s = 0;
+                for(auto &[_, c] : cnt) {
+                    ans -= s * c;
+                    s += c;
+                }
+            }
+            return ans;
+        }
+    };
