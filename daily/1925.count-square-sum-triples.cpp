4b5e826e1e55b04b184106bcee679ac2237ee669
c1uc <90602063+c1uc@users.noreply.github.com>
2025-12-11 09:16:59 +0800

    2025/12/7	1925. Count Square Sum Triples

---
+++ daily/1925.count-square-sum-triples.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    public:
+        int countTriples(int n) {
+            int ans = 0;
+            for(int a = 1;a < n;a++) {
+                for(int b = 1;b < n;b++) {
+                    int s = a * a + b * b;
+                    int c = sqrt(s);
+                    if(c * c == s && c <= n) ans++;
+                }
+            }
+            return ans;
+        }
+    };
