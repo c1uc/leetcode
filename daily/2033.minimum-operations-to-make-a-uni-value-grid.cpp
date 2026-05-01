baef0c5a2da0f837649eb703b1fb00b907f3f9b9
c1uc <coo5555553@gmail.com>
2026-05-01 00:00:06 +0000

    2026/5/1	2033. Minimum Operations to Make a Uni-Value Grid

--- daily/2033.minimum-operations-to-make-a-uni-value-grid.cpp
+++ daily/2033.minimum-operations-to-make-a-uni-value-grid.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        int ofs = grid[0][0] % x;
+        int ofs = grid[0][0] % x, n = grid.size() * grid[0].size();
         vector<int> flattened;
         for (auto& row : grid) {
             for (auto& cell : row) {
@@ -12,8 +12,8 @@ public:
                 flattened.emplace_back(cell);
             }
         }
-        sort(flattened.begin(), flattened.end());
-        int median = flattened[flattened.size() / 2];
+        nth_element(flattened.begin(), flattened.begin() + n / 2, flattened.end());
+        int median = flattened[n / 2];
         int ans = 0;
         for (auto& cell : flattened) {
             ans += abs(cell - median) / x;
