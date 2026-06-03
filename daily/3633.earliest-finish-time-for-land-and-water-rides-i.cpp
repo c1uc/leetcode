69337e94e3ecc32be1011788e96f6513df27b2e8
c1uc <coo5555553@gmail.com>
2026-06-03 00:00:07 +0000

    2026/6/3	3633. Earliest Finish Time for Land and Water Rides I

---
+++ daily/3633.earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& s1, vector<int>& d1, vector<int>& s2, vector<int>& d2) {
+        auto solve = [](vector<int>& s1, vector<int>& s2, vector<int>& d1, vector<int>& d2) -> int {
+            int t = 2001, res = 4001;
+            for(auto [s, d]: views::zip(s1, d1)) t = min(t, s + d);
+            for(auto [s, d]: views::zip(s2, d2)) res = min(res, max(t, s) + d);
+            return res;
+        };
+        return min(solve(s1, s2, d1, d2), solve(s2, s1, d2, d1));
+    }
+};
