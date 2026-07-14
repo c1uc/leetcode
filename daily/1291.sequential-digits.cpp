1d901ed4f6145658436c0c1b260b1b148b0f58e4
c1uc <coo5555553@gmail.com>
2026-07-14 00:00:07 +0000

    2026/7/14	1291. Sequential Digits

---
+++ daily/1291.sequential-digits.cpp
@@ -0,0 +1,8 @@
+const vector<int> v = {12, 23, 34, 45, 56, 67, 78, 89, 123, 234, 345, 456, 567, 678, 789, 1234, 2345, 3456, 4567, 5678, 6789, 12345, 23456, 34567, 45678, 56789, 123456, 234567, 345678, 456789, 1234567, 2345678, 3456789, 12345678, 23456789, 123456789};
+
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        return vector<int>(ranges::lower_bound(v, low), ranges::upper_bound(v, high));
+    }
+};
