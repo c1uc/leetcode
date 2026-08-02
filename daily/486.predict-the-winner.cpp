9085e3136c7814d13d74e0eaa4fba33c81489033
c1uc <coo5555553@gmail.com>
2026-08-02 00:00:06 +0000

    2026/8/2	486. Predict the Winner

---
+++ daily/486.predict-the-winner.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        vector dp = nums;
+        int n = nums.size();
+        for(int i = n - 2;i >= 0;i--) {
+            for(int j = i + 1;j < n;j++) {
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        return dp[n - 1] >= 0;
+    }
+};
